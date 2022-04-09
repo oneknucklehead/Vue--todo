f1ba2308bc41c31cf25c0e62cb3e2dbc6e626c66
Zoheb Ahmed <zohebcool1542@gmail.com>
2022-04-10 01:53:19 +0530

    add task functionality added

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button v-on:click="clickFunc()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    clickFunc() {
+      this.$emit('btn-click')
+    },
+  },
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? `Close` : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+export default {
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      default: 'hello world',
+    },
+    showAddTask: {
+      type: Boolean,
+    },
+  },
+  components: {
+    Button,
+  },
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 0 20px 0;
+}
+</style>
